5689723a5e138c8592d9fee62c678ab0166064ec
FurkanHome <FurkanHome@FURKANPC>
2020-09-01 18:20:08 +0300

    Add project files.

---
+++ SampleWebService/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebService.Models;
+
+namespace SampleWebService.Controllers
+{
+    [Produces("application/json")]
+    [Route("[action]")]
+    [ApiController]
+    [Authorize]
+    public class DefaultController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Test([FromBody] TestModel model)
+        {
+            return new JsonResult(new
+            {
+                success = true,
+                model.Id,
+                model.Name
+            });
+        }
+    }
+}
